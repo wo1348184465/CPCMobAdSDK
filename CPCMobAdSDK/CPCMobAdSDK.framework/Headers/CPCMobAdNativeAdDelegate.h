4a742fbf9081967560a132bba597f9625c02b1a1
丁昊 <dinghao@qutoutiao.net>
2018-12-19 21:41:38 +0800

    1.可以通过初始化 传递 no_baidu 不走百度逻辑 2.可以自定义界面

---
+++ CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCMobAdNativeAdDelegate.h
@@ -0,0 +1,56 @@
+//
+//  CPCMobAdNativeAdDelegate.h
+//  CPCMobAdSDK
+//
+//  Created by dinghao on 2018/12/17.
+//  Copyright © 2018年 dinghao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "CPCMobAdNativeAdObject.h"
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol CPCMobAdNativeAdDelegate <NSObject>
+/**
+ * 广告位id
+ */
+-(NSString*)adId;
+
+
+
+
+/**
+ 广告请求成功
+
+ @param nativeAds 请求成功的BaiduMobAdNativeAdObject
+ */
+- (void)cpcNativeAdObjectsSuccessLoad:(CPCMobAdNativeAdObject *)nativeAds;
+
+
+
+
+
+
+/**
+ 广告请求失败
+
+ @param reason 失败的BaiduMobAdNative , 失败的类型 BaiduMobFailReason
+ */
+- (void)cpcNativeAdsFailLoad:(NSError *)reason;
+
+
+/**
+ *  广告点击
+ */
+- (void)cpcNativeAdClicked:(UIView*)nativeAdView;
+
+/**
+ *  广告详情页关闭
+ */
+-(void)cpcDidDismissLandingPage:(UIView *)nativeAdView;
+
+
+@end
+
+NS_ASSUME_NONNULL_END
