8742ff3b56fa0be6ce53d005f5a47ca992ac51ee
丁昊 <dinghao@qutoutiao.net>
2019-06-05 10:56:25 +0800

    0.1.0.94.0604.341.3 dp_clk 写成了clk

--- CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
+++ CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
@@ -16,7 +16,7 @@
 #import "CPCInitConfig.h"
 
 
-#define CPCMOBADSDK_VERSION @"0.1.0.94.0604.341.2" // 互动, sketch
+#define CPCMOBADSDK_VERSION @"0.1.0.94.0604.341.3" // 互动, sketch
 
 
 NS_ASSUME_NONNULL_BEGIN
