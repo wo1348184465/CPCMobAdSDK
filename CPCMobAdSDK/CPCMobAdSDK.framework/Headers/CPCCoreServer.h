9f79eeeb478a8a4d72aaebe52153a2ef13e25591
丁昊 <dinghao@qutoutiao.net>
2019-06-11 14:48:49 +0800

    1.V3- V4 2.UI4.0

--- CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
+++ CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
@@ -16,7 +16,7 @@
 #import "CPCInitConfig.h"
 
 
-#define CPCMOBADSDK_VERSION @"0.1.0.94.0605.341.4" // 互动, sketch
+#define CPCMOBADSDK_VERSION @"0.1.1.0.0611.345.3" // 互动, sketch
 
 
 NS_ASSUME_NONNULL_BEGIN
