03e35b2ff645c6ad95a7777a1308af177e05bb36
丁昊 <dinghao@qutoutiao.net>
2019-06-11 15:11:56 +0800

    0.1.1.0.0611.345.4 1.增加接口判断 2.强制转换dp_clk

--- CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
+++ CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
@@ -16,7 +16,7 @@
 #import "CPCInitConfig.h"
 
 
-#define CPCMOBADSDK_VERSION @"0.1.1.0.0611.345.3" // 互动, sketch
+#define CPCMOBADSDK_VERSION @"0.1.1.0.0611.345.4" // 互动, sketch
 
 
 NS_ASSUME_NONNULL_BEGIN
