478de19000bcd5de46412ac6f83b6b17676215d3
丁昊 <dinghao@qutoutiao.net>
2019-06-14 22:25:17 +0800

    0.1.1.0.0614.345.13 百度打底高度修复

--- CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
+++ CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
@@ -16,7 +16,7 @@
 #import "CPCInitConfig.h"
 
 
-#define CPCMOBADSDK_VERSION @"0.1.1.0.0614.345.12" // 互动, sketch
+#define CPCMOBADSDK_VERSION @"0.1.1.0.0614.345.13" // 互动, sketch
 
 
 NS_ASSUME_NONNULL_BEGIN
