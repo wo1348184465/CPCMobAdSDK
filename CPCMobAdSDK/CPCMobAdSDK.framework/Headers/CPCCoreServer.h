c6b9d91e33c8d7671cb6f2dd1a65bdaf7300b15e
丁昊 <dinghao@qutoutiao.net>
2019-06-18 21:22:46 +0800

    0.1.1.0.0618.345.18 1.修复大图 title 字体不能放大 2.修复单图 位置错乱

--- CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
+++ CPCMobAdSDK/CPCMobAdSDK.framework/Headers/CPCCoreServer.h
@@ -16,7 +16,7 @@
 #import "CPCInitConfig.h"
 
 
-#define CPCMOBADSDK_VERSION @"0.1.1.0.0618.345.17" // 互动, sketch
+#define CPCMOBADSDK_VERSION @"0.1.1.0.0618.345.18" // 互动, sketch
 
 
 NS_ASSUME_NONNULL_BEGIN
